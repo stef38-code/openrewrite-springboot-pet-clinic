b4b2286369a8483797f5c069247096cfa4232fa9
rjdarkknight1 <hrjhonor@gmail.com>
2019-07-03 22:34:26 -0400

    project initialization

---
+++ src/test/java/com/example/springbootpetclinic/SpringbootPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springbootpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
