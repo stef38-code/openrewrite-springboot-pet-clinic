fd58e6b063e3919bab6851d99a40056120d2a930
rjdarkknight1 <hrjhonor@gmail.com>
2019-07-06 16:35:59 -0400

    Create modules for data and web. Closes #1.

--- pet-clinic-web/src/test/java/springbootpetclinic/SpringbootPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/springbootpetclinic/SpringbootPetClinicApplicationTests.java
@@ -1,12 +1,14 @@
-package com.example.springbootpetclinic;
+package springbootpetclinic;
 
+import com.example.springbootpetclinic.SpringbootPetClinicApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+// have to specify the test class
+@SpringBootTest(classes=SpringbootPetClinicApplication.class)
 public class SpringbootPetClinicApplicationTests {
 
     @Test
