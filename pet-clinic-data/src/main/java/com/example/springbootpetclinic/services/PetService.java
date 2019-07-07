f6d302b8b6fca298ac9b71bfd38359967e9782b7
rjdarkknight1 <hrjhonor@gmail.com>
2019-07-07 11:59:01 -0400

    Refactor service interfaces to common base interface. Closes #11.

--- pet-clinic-data/src/main/java/com/example/springbootpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/example/springbootpetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package com.example.springbootpetclinic.services;
 
 import com.example.springbootpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/com/example/springbootpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/example/springbootpetclinic/services/VetService.java
@@ -2,15 +2,6 @@ package com.example.springbootpetclinic.services;
 
 import com.example.springbootpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findByLastName(String lastName);
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
