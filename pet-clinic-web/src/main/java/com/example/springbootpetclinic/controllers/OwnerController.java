22730870715fbcadfeb481d2852bfec926517255
rjdarkknight1 <hrjhonor@gmail.com>
2019-07-08 21:53:42 -0400

    Implement Owner Index Page and Controller. Closes #14.

---
+++ pet-clinic-web/src/main/java/com/example/springbootpetclinic/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package com.example.springbootpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    public String listVets(){
+        return "owners/index";
+    }
+
+}
