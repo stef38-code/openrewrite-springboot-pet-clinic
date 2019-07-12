e029894e71b429d636825ad3b68d66e8ed5a2d87
rjdarkknight1 <hrjhonor@gmail.com>
2019-07-11 21:43:14 -0400

    Add owners list and updated controller and index page. Closes #17.

--- pet-clinic-web/src/main/java/com/example/springbootpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/example/springbootpetclinic/controllers/OwnerController.java
@@ -1,13 +1,22 @@
 package com.example.springbootpetclinic.controllers;
 
+import com.example.springbootpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 
