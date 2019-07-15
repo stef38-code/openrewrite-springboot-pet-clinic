0e2a111ff9aee7de2f8323311c5bb54a1bf183a4
rjdarkknight1 <hrjhonor@gmail.com>
2019-07-14 22:15:29 -0400

    Add Speciality entity and add to vets. Closes #28.

--- pet-clinic-data/src/main/java/com/example/springbootpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/example/springbootpetclinic/model/Vet.java
@@ -1,4 +1,17 @@
 package com.example.springbootpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
+
 }
