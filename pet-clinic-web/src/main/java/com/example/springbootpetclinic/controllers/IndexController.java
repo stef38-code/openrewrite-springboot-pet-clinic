231f451f2302e5d1f68224ca2ba6ca50e44d86a5
rjdarkknight1 <hrjhonor@gmail.com>
2019-07-15 22:29:48 -0400

    Fix three pages find owners, list vets, error page. Closes #45.

--- pet-clinic-web/src/main/java/com/example/springbootpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/com/example/springbootpetclinic/controllers/IndexController.java
@@ -8,8 +8,12 @@ public class IndexController {
 
     @RequestMapping({"", "/", "index", "index.html"})
     public String index() {
-
         return "index";
+    }
+
+    @RequestMapping("/oups")
+    public String oupsHandler(){
+        return "notimplemented";
     }
 
 }
