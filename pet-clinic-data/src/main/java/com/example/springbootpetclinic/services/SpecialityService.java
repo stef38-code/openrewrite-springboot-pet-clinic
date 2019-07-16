a566b1f35fb1bd9104b1283e70097c7c8c1f06d6
rjdarkknight1 <hrjhonor@gmail.com>
2019-07-15 21:25:20 -0400

    Add Speciality Map Service Impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/com/example/springbootpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.example.springbootpetclinic.services;
+
+import com.example.springbootpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
