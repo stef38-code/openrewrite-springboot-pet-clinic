d7b9276593b94d8ae359cdbf8ac48797b9e3361d
rjdarkknight1 <hrjhonor@gmail.com>
2019-07-18 22:45:43 -0400

    Update models to JPA entities, map relationships. Closes #36. Closes #37. Closes #38.

--- pet-clinic-data/src/main/java/com/example/springbootpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/example/springbootpetclinic/model/Speciality.java
@@ -1,7 +1,14 @@
 package com.example.springbootpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "descriptions")
     private String description;
 
     public String getDescription() {
