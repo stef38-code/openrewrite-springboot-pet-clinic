aa23005e43caa245afd9cc3e54ef81f00c50ac5d
rjdarkknight1 <hrjhonor@gmail.com>
2019-07-20 11:19:04 -0400

    Closes #48. Refactor for Project Lombok

--- pet-clinic-data/src/main/java/com/example/springbootpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/example/springbootpetclinic/model/Speciality.java
@@ -1,22 +1,21 @@
 package com.example.springbootpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
     @Column(name = "descriptions")
     private String description;
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 
 }
